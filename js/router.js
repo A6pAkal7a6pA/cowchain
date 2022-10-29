6335d185360dc18ba5bae5e3ca00926f85fc4861
Maksym Kuleshov <maksym_kuleshov@epam.com>
2022-10-29 19:22:07 +0300

    added routing

--- js/router.js
+++ js/router.js
@@ -22,7 +22,7 @@ const handleLocation = async () => {
 	let path = window.location.pathname.replace('/cowchain', '');
 	const route = routes[path];
 	if (route !== undefined) {
-		const html = await fetch(window.location.origin + '/cowchain' + route).then(data => data.text());
+		const html = await fetch(window.location.origin + '/cowchain/' + route).then(data => data.text());
 		document.getElementById('root').innerHTML = html;
 	}
 }
